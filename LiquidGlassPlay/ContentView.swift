c783a9540d737dbb4345b41bea7338d8348cf4cb
Ryan McLeod <ryanmcleod@gmail.com>
2025-07-03 18:28:55 +0200

    Initial Commit

---
+++ LiquidGlassPlay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiquidGlassPlay
+//
+//  Created by Ryan McLeod on 7/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiquidGlassPlay/LiquidGlassPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidGlassPlayApp.swift
+//  LiquidGlassPlay
+//
+//  Created by Ryan McLeod on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct LiquidGlassPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
